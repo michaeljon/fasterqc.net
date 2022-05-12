89bd698445b7aa77578fd14d05f8bef0dc1bd7bf
Michaeljon Miller <michaeljon.miller@outlook.com>
2022-05-12 13:52:10 -0700

    upping to net60

--- Program.cs
+++ Program.cs
@@ -15,15 +15,15 @@ namespace Ovation.FasterQC.Net
 
         private static readonly List<IQcModule> modules = new()
         {
-            // new BasicStatistics(),
-            // new KMerContent(),
-            // new NCountsAtPosition(),
-            // new PerPositionSequenceContent(),
-            // new PerSequenceGcContent(),
+            new BasicStatistics(),
+            new KMerContent(),
+            new NCountsAtPosition(),
+            new PerPositionSequenceContent(),
+            new PerSequenceGcContent(),
             new QualityDistributionByBase(),
-            // new MeanQualityDistribution(),
-            // new SequenceLengthDistribution(),
-            // new PerPositionQuality()
+            new MeanQualityDistribution(),
+            new SequenceLengthDistribution(),
+            new PerPositionQuality()
         };
 
         static void Main(string[] args)
